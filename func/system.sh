e02839fb80f93e7a1ab643c8938dd56e276fadc8
jbenages <jbinarys@gmail.com>
2018-11-19 15:19:17 +0100

    Add sessions manage functions and move create directories function

--- func/system.sh
+++ func/system.sh
@@ -15,6 +15,38 @@ function stopZombieProcess {
 
 }
 
+function getSessions {
+
+  declare LSESSION=""
+  
+  for LSESSION in "$CURRENTDIR"/data/db/*
+  do
+    echo -n $LSESSION | cut -d"." -f2
+  done
+ 
+}
+
+function existsSessions {
+
+  declare LSESSION=""
+  declare LSESSIONTOSEARCH=$1
+  
+  for LSESSION in "$CURRENTDIR"/data/db/*
+  do
+    LSESSION=$( echo $LSESSION | cut -d"." -f2 )
+    if [ "$LSESSION" = "$LSESSIONTOSEARCH" ];then
+      echo "$LSESSION"
+      exit 0
+    fi
+  done
+ 
+}
+
+function createDirectory {
+  mkdir -p $CURRENTDIR/data/{tmp,airmon,airodump,csv,db,oui}
+  mkdir -p $CURRENTDIR/data/tmp/{log,PID,csv,nmap}
+}
+
 function getManufacturerFromMAC {
   
   local LMAC=$1
