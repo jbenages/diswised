370e557bc522161fecb4aa79ab293035c77496de
jbenages <jbinarys@gmail.com>
2018-12-10 13:40:21 +0100

    Add number of android vulnerable clients in stats

--- func/stats.sh
+++ func/stats.sh
@@ -6,6 +6,7 @@ function getStats {
   declare LTOTALCLIENTS=0
   declare LTOTALVULNERABLECLIENTS=0
   declare LPERCENTAGE=""
+  declare LCLIENTANDROID=0
 
   LRESULT=$( query "select count(*) from client;" )
   if [ $? != 0 ];then
@@ -24,22 +25,28 @@ function getStats {
   LTOTALVULNERABLECLIENTS=$LRESULT
   LPERCENTAGE=$(( LTOTALVULNERABLECLIENTS*100/LTOTALCLIENTS ))
 
+  LRESULT=$( query "select count(*) from client where hostname like 'android%%';" )
+  if [ $? != 0 ];then
+    logOutput "alert" "Fail extract clients with android ($LRESULT) \n"
+    exit 1
+  fi
+  LCLIENTANDROID=$LRESULT
+
   echo ' ____  _              _              _ '
   echo '|  _ \(_)_____      _(_)___  ___  __| |'
   echo '| | | | / __\ \ /\ / / / __|/ _ \/ _` |'
   echo '| |_| | \__ \\ V  V /| \__ \  __/ (_| |'
   echo '|____/|_|___/ \_/\_/ |_|___/\___|\__,_|'
   echo ''
-
-	                                        
   echo -e "\e[1mGeneral Stats\e[0m"
-  echo -e "\tTotal audited clients: $LTOTALCLIENTS"
-  echo -e "\tTotal vulnerable clients: $LTOTALVULNERABLECLIENTS"
-  echo -e "\tPercentage vulnerable clients: \e[91m$LPERCENTAGE%\e[0m\n"
+  echo -e "\tTotal audited clients: \e[1m$LTOTALCLIENTS\e[0m"
+  echo -e "\tTotal vulnerable clients: \e[1m$LTOTALVULNERABLECLIENTS\e[0m"
+  echo -e "\tPercentage vulnerable clients: \e[91m$LPERCENTAGE%\e[0m"
+  echo -e "\tTotal vulnerable clients with android: \e[1m$LCLIENTANDROID\e[0m\n"
 
   LRESULT=$( prettyQuery "select manufacturer as Manufacturer,count(*) as 'Number clients' from client where hostname != '' and manufacturer != '' group by manufacturer order by count(*) desc;" )
   if [ $? != 0 ];then
-    logOutput "alert" "Fail extract percentage of clients and vulnerable clients ($LRESULT) \n"
+    logOutput "alert" "Fail extract vulnerable clients by manufacturer ($LRESULT) \n"
     exit 1
   fi
 
@@ -48,7 +55,7 @@ function getStats {
 
   LRESULT=$( prettyQuery "select attack.description as 'Type attack',count(client_attack.client_id) as 'Number clients' from client inner join client_attack on client.id = client_attack.client_id inner join attack on client_attack.attack_id = attack.id where client.hostname != '' group by attack.description;" )
   if [ $? != 0 ];then
-    logOutput "alert" "Fail extract percentage of clients and vulnerable clients ($LRESULT) \n"
+    logOutput "alert" "Fail extract vulnerable clients number by type attack ($LRESULT) \n"
     exit 1
   fi
 
